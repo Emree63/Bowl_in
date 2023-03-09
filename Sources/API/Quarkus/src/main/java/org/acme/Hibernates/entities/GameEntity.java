0c2b38605683cf233967d68a500e6cd57dda9c72
dadalmeida1 <David.D_ALMEIDA@etu.uca.fr>
2023-03-09 11:52:20 +0100

    fix merge error

--- Sources/API/Quarkus/src/main/java/org/acme/Hibernates/entities/GameEntity.java
+++ Sources/API/Quarkus/src/main/java/org/acme/Hibernates/entities/GameEntity.java
@@ -4,22 +4,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
-<<<<<<< HEAD
-import javax.persistence.Entity;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-@Entity
-=======
 
 import javax.persistence.*;
-@Entity
-@Table(name="games")
->>>>>>> bcd6335175194e1c12cc113060a02e202da0af79
+
+// @Entity
+// @Table(name = "games")
 public class GameEntity {
 
     @Id
