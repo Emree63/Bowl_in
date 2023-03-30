1573c81cd47824d4ec86dbb7afc61c45a88f57a0
dadalmeida1 <David.D_ALMEIDA@etu.uca.fr>
2023-03-30 22:43:38 +0200

    push for docker

--- Sources/API/Quarkus/src/test/java/org/acme/UserControllerTest.java
+++ Sources/API/Quarkus/src/test/java/org/acme/UserControllerTest.java
@@ -1,7 +1,11 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -9,12 +13,27 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class UserControllerTest {
 
-    @Test
-    public void testUserEndpoint() {
-        given()
-                .when().get("/users/1")
-                .then()
-                .statusCode(200);
-    }
+    // @Test
+    // public void testUserEndpoint() {
+    // given()
+    // .when().get("/users/1")
+    // .then()
+    // .statusCode(200);
+    // }
+
+    // @Test
+    // @DisplayName("Test GET /users/{id}")
+    // @ParameterizedTest(name = "Test with user ID {0}")
+    // @CsvSource({
+    // "2, Bob",
+    // "3, Charlie"
+    // })
+    // public void testGetUserById(Long id, String expectedName) {
+    // given()
+    // .when().get("/users/{id}", id)
+    // .then()
+    // .statusCode(200)
+    // .body("name", equalTo(expectedName));
+    // }
 
 }
